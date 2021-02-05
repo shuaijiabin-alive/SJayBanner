6cc075b43f606cf85a7b0b0abc9f8add97463509
shuaijiabin <1194185701@qq.com>
2021-02-05 15:30:28 +0800

    增加UE富文本编辑器

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />567567567
+    <UEditer></UEditer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import UEditer from "@/components/UEditer.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    UEditer
+  },
+  data() {
+    return {
+    }
   }
 };
 </script>
